4ea9ed385521167c19c2383d6a6e5511b8a7f3e4
mlimaloureiro <mdiasloureiro@gmail.com>
2016-01-09 23:21:59 +0000

    add save and clear method on repository; add some util methods and tests

--- repository.go
+++ repository.go
@@ -10,23 +10,24 @@ import (
 // operations. Example implementations of an RepositoryProvider might be a simple
 // csv file, sql, mongo...
 type RepositoryInterface interface {
-	save(tasks Tasks) bool
+	save(task Task) bool
 	load() Tasks
 }
 
-// TaskCsv is a type with the path of the file to be readed
-type TaskCsv struct {
+// TaskCsvRepository is a type with the path of the file to be readed
+type TaskCsvRepository struct {
 	Path string
 }
 
 // FileRepository implementation of RepositoryInterface for simple .csv files
 // each line: identifier,action,at
-func (csvPath TaskCsv) load() Tasks {
-	csvFile, err := os.Open(csvPath.Path)
+func (csvRepository TaskCsvRepository) load() Tasks {
+	csvFile, err := os.Open(csvRepository.Path)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer csvFile.Close()
 
 	reader := csv.NewReader(csvFile)
 	reader.FieldsPerRecord = -1
@@ -38,11 +39,35 @@ func (csvPath TaskCsv) load() Tasks {
 	}
 
 	tasks := Tasks{}
-	// sanity check, display to standard output
 	for _, line := range rawCsvData {
-		tasks.AddItem(Task{Identifier: line[0], Action: line[1], At: line[2]})
+		tasks.addItem(Task{Identifier: line[0], Action: line[1], At: line[2]})
+	}
+
+	return tasks
+}
+
+func (csvRepository TaskCsvRepository) save(task Task) bool {
+	csvFile, err := os.OpenFile(csvRepository.Path, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer csvFile.Close()
 
-	return tasks
+	writer := csv.NewWriter(csvFile)
+	writer.Write(task.toArrayString())
+	writer.Flush()
+
+	return true
+}
+
+func (csvRepository TaskCsvRepository) clear() bool {
+	csvFile, err := os.OpenFile(csvRepository.Path, os.O_TRUNC|os.O_WRONLY, 0600)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer csvFile.Close()
+
+	return true
 }
--- tasks.go
+++ tasks.go
@@ -13,7 +13,23 @@ type Task struct {
 }
 
 // AddItem add a new task to Tasks items field
-func (tasks *Tasks) AddItem(item Task) []Task {
+func (tasks *Tasks) addItem(item Task) []Task {
 	tasks.Items = append(tasks.Items, item)
 	return tasks.Items
+}
+
+func (task Task) toArrayString() []string {
+	return []string{task.Identifier, task.Action, task.At}
+}
+
+func (task Task) getIdentifier() string {
+	return task.Identifier
+}
+
+func (task Task) getAction() string {
+	return task.Action
+}
+
+func (task Task) getAt() string {
+	return task.At
 }
